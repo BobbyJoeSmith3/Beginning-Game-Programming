2577815bbe1d77f659ec63ac178dcaec5c40e89e
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-09-16 00:38:56 -0400

    Started two C# projects in Xamarin Studios IDE

---
+++ projects/FirstCSharpProgram/FirstCSharpProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstCSharpProgram
+{
+	/// <summary>
+	/// Class that prints message
+	/// </summary>
+	class MainClass
+	{
+		/// <summary>
+		/// Main method to print message
+		/// </summary>
+		/// <param name="args">The command-line arguments.</param>
+		public static void Main (string[] args)
+		{
+			// print a message
+			Console.WriteLine ("Hello World!");
+
+			Console.WriteLine ();
+		}
+	}
+}
